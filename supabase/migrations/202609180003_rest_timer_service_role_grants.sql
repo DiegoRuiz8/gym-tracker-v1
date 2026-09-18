c4347f6689c3a21570c9965b2fe12fbab82a1d17
Diego <diego.ruiz4125@alumnos.udg.mx>
2026-09-18 13:21:49 -0600

    grant rest timer tables to service role

---
+++ supabase/migrations/202609180003_rest_timer_service_role_grants.sql
@@ -0,0 +1,4 @@
+grant usage on schema public to service_role;
+
+grant all privileges on table public.push_subscriptions to service_role;
+grant all privileges on table public.rest_timer_push_jobs to service_role;
